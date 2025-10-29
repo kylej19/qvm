57ff611e64e9fe7c2828b32e29aab4af55165f06
kyle johnson <kmj19@tuta.io>
2025-10-29 11:04:18 -0500

    Update boot.sh

--- debian/boot.sh
+++ debian/boot.sh
@@ -1,1 +1,7 @@
-qemu-system-x86_64 -m 8192 -cpu max -drive file=/Users/kyle/gh/qvm/debian/disk.qcow2,media=disk,if=virtio -nic user,model=virtio -display cocoa -accel hvf
+qemu-system-x86_64 \
+-m 8192 \
+-accel hvf \
+-cpu max \
+-drive file=./vdisk.qcow2,media=disk,if=virtio \
+-nic user,model=virtio \
+-display cocoa

6ea738ae9cb818553cfa75992c9392bde8d21a6d
kyle johnson <kmj19@tuta.io>
2025-10-30 09:49:01 -0500

    streamlined-again

--- init.sh
+++ init.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-# will fail if already exists on purpose
-mkdir -p ./blank-vm
+
+URL="$1"
+SIZE="$2"
+
 echo "> mkdir ../blank-vm ..."
 # copies all needed scripts
-cp ./boot.sh ../blank-vm/boot.sh
-cp ./pull.sh ../blank-vm/pull.sh
-echo "> cp ./boot.sh ../blank-vm/boot.sh ..."
-echo "> cp ./pull.sh ../blank-vm/pull.sh ..."
+cp ./boot.sh ../base-vm/boot.sh
+cp ./pull.sh ../base-vm/pull.sh
+echo "> cp ./boot.sh ../base-vm/boot.sh ..."
+echo "> cp ./pull.sh ../basek-vm/pull.sh ..."
 # and moves into the directory
-cd ../blank-vm
+cd ../base-vm
 
+
+echo "> downloading qcow2 from $1 ..."
+curl -Lo vdisk.qcow2 "$1"
+echo "> resizing image vdisk.qcow2 to $2 ..."
+qemu-img resize vdisk.qcow2 $SIZE
--- pull.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-URL="$1"
-SIZE="$2"
-
-echo "> downloading qcow2 from $1 ..."
-curl -Lo vdisk.qcow2 "$1"
-echo "> resizing image vdisk.qcow2 to $2 ..."
-qemu-img resize vdisk.qcow2 $SIZE
